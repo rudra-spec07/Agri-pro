07436479c7f41ef20cb95e79b0fc674cc27f8021
rudrakashchoubey <rudrachoubey70@gmail.com>
2026-02-21 21:33:12 +0530

    updated web site

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function showMessage() {
     alert("Welcome to Agri Grow Solutions!");
 }
+
+// optional JavaScript can be added later for enhancements (e.g. active link highlighting)
+// currently navigation handled by regular anchor behavior with smooth scroll enabled by CSS
+
+// keep showMessage function separate
